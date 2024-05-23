6fd9208bb672182b464c3b15b0384e114aece90f
Kaamandkomposed <kaamandkomposed@gmail.com>
2024-05-23 15:52:05 +0200

    Add and configure Daisy UI

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,12 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('daisyui')
+	],
+	daisyui: {
+		themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+		darkTheme: "dracula", // name of one of the included themes for dark mode
+	  },
 }
